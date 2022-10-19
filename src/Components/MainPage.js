18eecbdf1916caf265bad130cd1f2da1ebb2b4d6
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-19 13:41:44 +0100

    charts done

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -57,11 +57,11 @@ const MainPage = () => {
                         </div>
 
                         <div className="album">
-                            <img src="/assets/golden-album.png" alt="album" />
+                            <img src="/assets/rEggae-album.png" alt="album" />
                             <span>
-                                <p className="music">Golden age of 80s</p>
-                                <p className="name">sean swadder</p>
-                                <p className="time">2:45:45</p>
+                                <p className="music">Reggae "n" blues</p>
+                                <p className="name">Dj Yk mule</p>
+                                <p className="time">1:02:45</p>
                             </span>
                             <span>
                             <i class='bx bx-heart'></i>
@@ -69,11 +69,11 @@ const MainPage = () => {
                         </div>
 
                         <div className="album">
-                            <img src="/assets/golden-album.png" alt="album" />
+                            <img src="/assets/tunes-album.png" alt="album" />
                             <span>
-                                <p className="music">Golden age of 80s</p>
-                                <p className="name">sean swadder</p>
-                                <p className="time">2:45:45</p>
+                                <p className="music">Tomorrow's tunes</p>
+                                <p className="name">Obi Datti</p>
+                                <p className="time">2:01:25</p>
                             </span>
                             <span>
                             <i class='bx bx-heart'></i>
